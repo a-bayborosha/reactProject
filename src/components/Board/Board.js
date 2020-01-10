666c55169961e1a96b10987ff55d3d9d3b6bd097
a-bayborosha <anton.bayborosha@gmail.com>
2020-01-10 13:11:43 +0100

    changing "Square" to the FUNCTION COMPONENT

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,8 +3,30 @@ import Square from '../Square/Square'
 import './Board.css'
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
+    //by Mouse click on Square component
+    handleClick(i) {
+        //create copy of the squares array
+        const squares = this.state.squares.slice()
+        //assigne a new value to [i]- ellement of coppied array
+        squares[i] = 'X'
+        //renew the state
+        this.setState({squares})
+    }
+
+    //rendering Square component
     renderSquare(i) {
-        return <Square value={i} />
+        return <Square 
+                    value={this.state.squares[i]}
+                    onClick={() => this.handleClick(i)}
+                />
     }
     render() {
     const status = 'Next player: X'
@@ -12,6 +34,7 @@ class Board extends React.Component {
         return(
             <div>
                 <div className='status'>{status}</div>
+
                 <div className='board-row'>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -29,6 +52,7 @@ class Board extends React.Component {
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
                 </div>
+
             </div>
         
         )
